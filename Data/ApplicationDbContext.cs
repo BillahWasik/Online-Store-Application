cd9809a83ee3d739ddf07b874001407fb8e2a038
Wasik Billah <51834225+BillahWasik@users.noreply.github.com>
2022-12-20 01:59:42 +0600

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Online_Store_Application.Data
+{
+    public class ApplicationDbContext : DbContext 
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
